180d148935fd9ac190c904b5e6ada348606e4156
msalafia <msalafia@seamgroup.com>
2021-12-04 10:09:57 -0300

    Layout design

---
+++ src/AngryDetection/TwoSense.Angry.UI/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace TwoSense.Angry.UI
+{
+    partial class FrmSendEmail
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbTo = new System.Windows.Forms.TextBox();
+            this.tbSubject = new System.Windows.Forms.TextBox();
+            this.tbBody = new System.Windows.Forms.TextBox();
+            this.btSend = new System.Windows.Forms.Button();
+            this.lbTo = new System.Windows.Forms.Label();
+            this.lbSubject = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbTo
+            // 
+            this.tbTo.Location = new System.Drawing.Point(208, 42);
+            this.tbTo.Name = "tbTo";
+            this.tbTo.Size = new System.Drawing.Size(748, 47);
+            this.tbTo.TabIndex = 1;
+            // 
+            // tbSubject
+            // 
+            this.tbSubject.Location = new System.Drawing.Point(213, 110);
+            this.tbSubject.Name = "tbSubject";
+            this.tbSubject.Size = new System.Drawing.Size(743, 47);
+            this.tbSubject.TabIndex = 3;
+            // 
+            // tbBody
+            // 
+            this.tbBody.AcceptsReturn = true;
+            this.tbBody.AcceptsTab = true;
+            this.tbBody.Location = new System.Drawing.Point(213, 209);
+            this.tbBody.Multiline = true;
+            this.tbBody.Name = "tbBody";
+            this.tbBody.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbBody.Size = new System.Drawing.Size(1143, 866);
+            this.tbBody.TabIndex = 4;
+            // 
+            // btSend
+            // 
+            this.btSend.Location = new System.Drawing.Point(1083, 42);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(273, 115);
+            this.btSend.TabIndex = 5;
+            this.btSend.Text = "Send";
+            this.btSend.UseVisualStyleBackColor = true;
+            // 
+            // lbTo
+            // 
+            this.lbTo.AutoSize = true;
+            this.lbTo.Location = new System.Drawing.Point(84, 48);
+            this.lbTo.Name = "lbTo";
+            this.lbTo.Size = new System.Drawing.Size(56, 41);
+            this.lbTo.TabIndex = 0;
+            this.lbTo.Text = "To:";
+            // 
+            // lbSubject
+            // 
+            this.lbSubject.AutoSize = true;
+            this.lbSubject.Location = new System.Drawing.Point(84, 113);
+            this.lbSubject.Name = "lbSubject";
+            this.lbSubject.Size = new System.Drawing.Size(123, 41);
+            this.lbSubject.TabIndex = 2;
+            this.lbSubject.Text = "Subject:";
+            // 
+            // FrmSendEmail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1578, 1158);
+            this.Controls.Add(this.lbSubject);
+            this.Controls.Add(this.lbTo);
+            this.Controls.Add(this.btSend);
+            this.Controls.Add(this.tbBody);
+            this.Controls.Add(this.tbSubject);
+            this.Controls.Add(this.tbTo);
+            this.Name = "FrmSendEmail";
+            this.Text = "Send Email";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbTo;
+        private System.Windows.Forms.TextBox tbSubject;
+        private System.Windows.Forms.TextBox tbBody;
+        private System.Windows.Forms.Button btSend;
+        private System.Windows.Forms.Label lbTo;
+        private System.Windows.Forms.Label lbSubject;
+    }
+}
+
